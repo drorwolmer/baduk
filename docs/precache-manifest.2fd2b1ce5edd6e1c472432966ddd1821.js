a1663877ccb3be7f2ee5b1a82b177bd4cec53c84
dror <dror@cognigo.com>
2020-05-20 23:55:30 +0300

    t

---
+++ docs/precache-manifest.2fd2b1ce5edd6e1c472432966ddd1821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4854426f8999693eb4bc406ea2a770f",
+    "url": "/baduk/index.html"
+  },
+  {
+    "revision": "e89c1f512d5452414daf",
+    "url": "/baduk/static/css/main.3aa9d5ea.chunk.css"
+  },
+  {
+    "revision": "e1418e33e3e911226657",
+    "url": "/baduk/static/js/2.45bd8a8c.chunk.js"
+  },
+  {
+    "revision": "53995d46135c54a0169558d7c80d99a9",
+    "url": "/baduk/static/js/2.45bd8a8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89c1f512d5452414daf",
+    "url": "/baduk/static/js/main.8abba233.chunk.js"
+  },
+  {
+    "revision": "54b1d37f7b0b78e206b1",
+    "url": "/baduk/static/js/runtime-main.fa55bcdb.js"
+  }
+]);
