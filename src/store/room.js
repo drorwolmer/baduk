d160609df8a8264db48f2e3557bd2376c5e8a698
asaf <asaf@ddaylabs.com>
2020-05-21 18:35:02 +0300

    - save current room in local storage and get it on load - fix bubble css in side room

--- src/store/room.js
+++ src/store/room.js
@@ -1,17 +1,34 @@
-import { makeReducer } from '../utils'
+import { makeReducer, getFromLocalStorage } from '../utils'
 import { ROOMS } from '../consts'
 
-const initialRoom = window.location.href.indexOf('toilet') > -1 ? ROOMS.toilet : ROOMS.block
+export const setInitialRoom = () => {
+    // try url param
+    const urlParams = new URLSearchParams(window.location.search)
+    let roomName = urlParams.get('room')
+    if (!roomName) {
+        // try local storage
+        roomName = getFromLocalStorage('ROOM_NAME')
+    }
+    if (!roomName) {
+      // set default
+        roomName = 'block'
+    }
 
-export const setRoom = room => ({
-  type: 'SET_ROOM',
-  payload: { room },
-})
+    return setRoom(ROOMS[roomName])
+}
+
+export const setRoom = room => {
+    window.localStorage.setItem('ROOM_NAME', room.roomName)
+    return {
+        type: 'SET_ROOM',
+        payload: { room },
+    }
+}
 
 export const getRoom = state => state.room
 
 const roomReducer = makeReducer({
-  SET_ROOM: (state, action) => action.payload.room,
-}, initialRoom)
+    SET_ROOM: (state, action) => action.payload.room,
+}, {})
 
 export default roomReducer
