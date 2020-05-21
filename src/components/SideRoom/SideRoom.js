0236ce265d2fc8bc74e33c7da3110dd0f0a40e65
asaf <asaf@ddaylabs.com>
2020-05-21 21:02:19 +0300

    toilet page layout

--- src/components/SideRoom/SideRoom.js
+++ src/components/SideRoom/SideRoom.js
@@ -1,40 +1,47 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import _ from 'lodash'
 import classNames from 'classnames'
 import { useSelector } from 'react-redux'
-import UserList from '../UserDisplay/UserList'
 import { getLocalUser, getUsersByActiveRoom } from '../../store/users'
 import { joinSideRoom } from '../../modules/meeting'
 import './SideRoom.scss'
+import UserDisplay from '../UserDisplay'
 
 export default ({ name, maxSeats }) => {
 
-  const users = useSelector(getUsersByActiveRoom(name))
-  // console.warn('sideRoomUsers=' + JSON.stringify(users))
+    const users = useSelector(getUsersByActiveRoom(name))
 
-  const userCount = _.size(users)
-  const emptySeatCount = maxSeats - userCount
+    const userCount = _.size(users)
+    const emptySeatCount = maxSeats - userCount
 
-  const localUser = useSelector(getLocalUser)
+    const localUser = useSelector(getLocalUser)
 
-  const localUserInRoom = localUser.activeRoom === name
+    const localUserInRoom = localUser.activeRoom === name
 
-  const onFreeSeatClick = () => {
-    !localUserInRoom && joinSideRoom(name)
-  }
+    const onFreeSeatClick = () => {
+        !localUserInRoom && joinSideRoom(name)
+    }
 
-  return (
-    <div className="side-room" onClick={e => e.stopPropagation()}>
-      <div className="bg"/>
-      <UserList users={users} roomName={name} isAudioActive={localUserInRoom}/>
-      {_.map(Array(emptySeatCount), (o, i) => {
-        return (
-          <div className={classNames("user-display free-seat")} onClick={onFreeSeatClick}>
-            <div className="id">Speak up</div>
-            <div className="in"/>
-          </div>
-        )
-      })}
-    </div>
-  )
+    const renderUser = useCallback(user => (
+        <UserDisplay key={`user-display-${user.id}`} {...user} isAudioActive={localUserInRoom}/>
+    ), [users])
+
+    return (
+        <div className="side-room" onClick={e => e.stopPropagation()}>
+            <div className="bg"/>
+            {_.map(_.orderBy(users, 'id'), user => (
+                <div className="seat">{renderUser(user)}</div>
+            ))}
+            {_.map(Array(emptySeatCount), (o, i) => {
+                return (
+                    <div className="seat">
+                        <div className={classNames('user-display free-seat')} onClick={onFreeSeatClick}>
+                            <div className="id">Speak up</div>
+                            <div className="in"/>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
