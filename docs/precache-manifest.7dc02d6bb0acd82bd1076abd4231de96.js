10812c097edbd7babcee9805568e7a0f7dac3cfd
asaf <asaf@ddaylabs.com>
2020-05-21 18:09:22 +0300

    DEPLOY

---
+++ docs/precache-manifest.7dc02d6bb0acd82bd1076abd4231de96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcefe34a4b5191305a911bb9b1fedcd5",
+    "url": "/baduk/index.html"
+  },
+  {
+    "revision": "e1b3a26059100f6f4b12",
+    "url": "/baduk/static/css/main.deefe15b.chunk.css"
+  },
+  {
+    "revision": "ee0bb91481de2cd988ec",
+    "url": "/baduk/static/js/2.f68147be.chunk.js"
+  },
+  {
+    "revision": "53995d46135c54a0169558d7c80d99a9",
+    "url": "/baduk/static/js/2.f68147be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b3a26059100f6f4b12",
+    "url": "/baduk/static/js/main.cf0013df.chunk.js"
+  },
+  {
+    "revision": "54b1d37f7b0b78e206b1",
+    "url": "/baduk/static/js/runtime-main.fa55bcdb.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/baduk/precache-manifest.5ea1d113d0ed5c47d06806a600042924.js"
+  "/baduk/precache-manifest.7dc02d6bb0acd82bd1076abd4231de96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/baduk/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
