c0f84fc64f86a90774345d4ec3f1e50184487a48
asaf <asaf@ddaylabs.com>
2020-05-22 18:40:27 +0300

    DEPLOY

---
+++ docs/precache-manifest.5914274813ec4b74435b1999ffaafdb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0c7074f3941a151d81c985552f0d56",
+    "url": "/baduk/index.html"
+  },
+  {
+    "revision": "0b8d4be58b079cb2f513",
+    "url": "/baduk/static/css/main.c814fb07.chunk.css"
+  },
+  {
+    "revision": "0b7e25df629b833b7500",
+    "url": "/baduk/static/js/2.d5bc56f6.chunk.js"
+  },
+  {
+    "revision": "53995d46135c54a0169558d7c80d99a9",
+    "url": "/baduk/static/js/2.d5bc56f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8d4be58b079cb2f513",
+    "url": "/baduk/static/js/main.2b1351a5.chunk.js"
+  },
+  {
+    "revision": "54b1d37f7b0b78e206b1",
+    "url": "/baduk/static/js/runtime-main.fa55bcdb.js"
+  }
+]);
